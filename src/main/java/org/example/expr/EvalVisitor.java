9b4e068d84529f530b0a063e6fe974bf55865839
Haris Skiadas <cskiadas@gmail.com>
2025-03-14 12:12:45 -0400

    fill in evalvisitor methods

--- src/main/java/org/example/expr/EvalVisitor.java
+++ src/main/java/org/example/expr/EvalVisitor.java
@@ -3,32 +3,45 @@ package org.example.expr;
 import org.example.env.Env;
 import org.example.value.Value;
 
+import java.util.Optional;
+
 public class EvalVisitor implements Visitor<Value, Env<Value>> {
     public Value visit(BoolExpr expr, Env<Value> env) {
-        return null;
+        return Value.bool(expr.value);
     }
 
     public Value visit(CondExpr expr, Env<Value> env) {
-        return null;
+        boolean test = expr.cond.eval(env).asBoolean();
+        if (test) return expr.trueCase.eval(env);
+        else return expr.falseCase.eval(env);
     }
 
     public Value visit(GreaterExpr expr, Env<Value> env) {
-        return null;
+        double v1 = expr.e1.eval(env).asDouble();
+        double v2 = expr.e2.eval(env).asDouble();
+        return Value.bool(v1 > v2);
     }
 
     public Value visit(LetExpr expr, Env<Value> env) {
-        return null;
+        Value v = expr.e1.eval(env);
+        Env<Value> env2 = Env.extend(env);
+        env2.setValue(expr.symbol, v);
+        return expr.e2.eval(env2);
     }
 
     public Value visit(NumExpr expr, Env<Value> env) {
-        return null;
+        return Value.dbl(expr.value);
     }
 
     public Value visit(PlusExpr expr, Env<Value> env) {
-        return null;
+        double v1 = expr.e1.eval(env).asDouble();
+        double v2 = expr.e2.eval(env).asDouble();
+        return Value.dbl(v1 + v2);
     }
 
     public Value visit(VarExpr expr, Env<Value> env) {
-        return null;
+        Optional<Value> vOpt = env.getValue(expr.symbol);
+        if (vOpt.isPresent()) return vOpt.get();
+        throw new RuntimeException("Unknown variable: " + expr.symbol);
     }
 }
